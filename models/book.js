47049c14e12b1e67ece16f85945d4999960922b1
SaiKUndurthi <sai.undurthi@gmail.com>
2018-09-22 19:19:20 -0700

    Added books schema.

---
+++ models/book.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+
+// Schema
+var bookSchema = mongoose.Schema({
+	title:{
+		type: String,
+		required: true
+	},
+	genre:{
+		type: String,
+		required: true
+	},
+	description:{
+		type: String
+	},
+	author:{
+		type: String,
+		required: true
+	},
+	publisher:{
+		type: String
+	},
+	pages:{
+		type: String
+	},
+	image_url:{
+		type: String
+	},
+	buy_url:{
+		type: String
+	},
+	create_date:{
+		type: Date,
+		default: Date.now
+	}
+});
+
+var Book = module.exports = mongoose.model('Book',bookSchema);
+// Get Genres
+module.exports.getBooks = (callback, limit) => {
+	Book.find(callback).limit(limit);
+}
